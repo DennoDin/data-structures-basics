5d659d00f1a0f3f1a65129f592d1f23d2441bdf6
Code Chrysalis <codechrysalis@Codes-Mac-mini-2.local>
2018-07-16 18:08:39 +0900

    Created methods for addNode, addEdgem and removeNode for Graph
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Michael Arnold <arnold.michael91@gmail.com>

--- src/Graph.js
+++ src/Graph.js
@@ -2,7 +2,28 @@ class Graph {
   constructor() {
     this.nodes = {};
   }
+
+  addNode(value) {
+    this.nodes[value] = [];
+  }
+
+  addEdge(node1, node2) {
+    this.nodes[node1].push(node2);
+    this.nodes[node2].push(node1);
+  }
+
+  removeNode(node) {
+    for (let key in this.nodes) {
+      let index = this.nodes[key].indexOf(node);
+      if (index !== -1) {
+        this.nodes[key].splice(index, 1);
+      }
+    }
+    delete this.nodes[node];
+  }
 }
+
+module.exports = Graph;
 
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
