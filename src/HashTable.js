c5ee30ff0a4037a624ff02353c5f18b45dd59e1e
Code Chrysalis <codechrysalis@Codes-Mac-mini-2.local>
2018-07-17 10:26:37 +0900

    Completed hash table basic requirements
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Michael Arnold <arnold.michael91@gmail.com>

--- src/HashTable.js
+++ src/HashTable.js
@@ -4,17 +4,23 @@ class HashTable {
   constructor(limit = 8) {
     this.limit = limit;
     this.storage = ControlledArray(this.limit);
+    return this;
   }
 
   insert(key, value) {
-    const index = simpleHash(k, this.limit);
+    const index = simpleHash(key, this.limit);
+    this.storage.set(index, value);
   }
 
   retrieve(key) {
-    const idx = simpleHash(k, this.limit);
+    const index = simpleHash(key, this.limit);
+    return this.storage.get(index);
   }
 
-  remove(key) {}
+  remove(key) {
+    const index = simpleHash(key, this.limit);
+    this.storage.set(index, null);
+  }
 }
 
 module.exports = HashTable;
